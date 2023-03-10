2b05cde3020b5e4b49f60973487a16c38f617522
RoanMacmillan <hibee94@gmail.com>
2023-03-10 16:58:31 +0000

    small fix

--- src/assets/components/CountryCard/CountryCard.jsx
+++ src/assets/components/CountryCard/CountryCard.jsx
@@ -3,34 +3,24 @@ import "./CountryCard.css";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 const CountryCard = ({ countries }) => {
-
-
   return (
     <div className="countryContainer">
       {countries.map((country) => (
         <Link to={`/countries/${country.alpha3Code}`} key={country.alpha3Code}>
-        <div
-          className="countryCard"
-          key={country.alpha3Code}
-          
-        >
-          <div className="imgContainer">
-            <img src={country.flag} alt={`Flag of ${country.name}`} />
-          </div>
+          <div className="countryCard" key={country.alpha3Code}>
+            <div className="imgContainer">
+              <img src={country.flag} alt={`Flag of ${country.name}`} />
+            </div>
 
-          <div className="infoContainer">
-            <h2>{country.name}</h2>
-            <p>
-              Population: <span>{country.population}</span>
-            </p>
-            <p>
-              Region: <p>{country.region}</p>
-            </p>
-            <p>
-              Capital: <p>{country.capital}</p>
-            </p>
+            <div className="infoContainer">
+              <h2>{country.name}</h2>
+              <p>Population: {country.population}</p>
+
+              <p>Region: {country.region}</p>
+
+              <p>Capital: {country.capital}</p>
+            </div>
           </div>
-        </div>
         </Link>
       ))}
     </div>
