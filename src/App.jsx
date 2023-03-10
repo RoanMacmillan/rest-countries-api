dda742de9175fed7637bb2d365d71fb8a6f8b6ff
RoanMacmillan <hibee94@gmail.com>
2023-03-10 16:42:19 +0000

    small style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           <div className="headerWrapper">
           <h1>Where in the world?</h1>
 
-          <button onClick={handleToggle}>
+          <button type='button' className="headerBtn" onClick={handleToggle}>
             <img src={darkMode ? Sun : Moon} className="moon" alt="Moon"></img>
             <span>{darkMode ? "Light Mode" : "Dark Mode"}</span>
           </button>
